97b03169841fa3885bf7ecdbff136b5d4c56b9f8
Iain White <iain@intelligentgrowthsolutions.com>
2024-02-27 13:52:46 +0000

    Update setup.sh script to include zsh plugins and provide setup completion message

--- setup.sh
+++ setup.sh
@@ -93,3 +93,5 @@ sudo git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
+
+echo Setup complete. Run 'p10k configure' to configure powerlevel10k to your taste then restart your terminal.
